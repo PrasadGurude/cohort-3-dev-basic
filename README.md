d063dd456beb1cfb8367b67b6b15dbd5febeb39c
Prasad Gurude <prasadgurude76@gmail.com>
2025-03-22 13:26:56 +0530

    -

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 <!-- js is single threaded lang -->
 <!-- rust is muti threaded -->
 <!-- there are three way for memory managemetn 
-    1. Garbage collector   
+    1. Garbage collector  - used by Java and js
     2. Manual 
     3. The rust way-->
---
+++ index.js
@@ -0,0 +1,26 @@
+let firstname = "John"
+const age = 30;
+var isStudent = true;
+
+console.log(firstname);
+console.log(age);
+console.log(isStudent);
+
+isStudent = "hi"
+
+console.log(isStudent);
+
+// operators
+
+console.log("-------------operators-------------")
+
+console.log("5"+"3");
+
+console.log("-------------experiment-------------")
+
+let a = 5;
+let b = a;
+b--;
+console.log(a)
+console.log(b)
+
