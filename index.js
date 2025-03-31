638875c8a9dbf00f1690ee0f402e9b07afd5cf46
Prasad Gurude <prasadgurude76@gmail.com>
2025-03-31 16:35:06 +0530

    understood promises

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const fs = require('fs')
 
-// this is the syncronance approch
-const data = fs.readFileSync("a.txt","utf-8")
-console.log(data)
+// // this is the syncronance approch
+// const data = fs.readFileSync("a.txt","utf-8")
+// console.log(data)
 
-//this is the asyncronance approch
-fs.readFile('a.txt','utf-8', (err , data)=>{
-    console.log(data)
-})
+// //this is the asyncronance approch
+// fs.readFile('a.txt','utf-8', (err , data)=>{
+//     console.log(data)
+// })
+
+async function random(r){
+    const cont = await fs.readFile('a.txt','utf-8')
+    console.log(cont);
+    
+    r(); 
+}
+
+function callback(){
+    console.log("hi");  
+}
+
+let p = new Promise(random);
+p.then(callback)
